bb2c2914b3185c3d360ac220997e8b01f9e2c4c4
edsonlb <ti_edson@yahoo.com.br>
2016-05-09 16:41:05 +0200

    Page 31 - Adding module debug

---
+++ module/Debug/config/module.config.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Debug;
+
+return array(
+    'router' => array(
+        'routes' => array(
+            'home' => array(
+                'type' => 'Zend\Mvc\Router\Http\Literal',
+                'options' => array(
+                    'route'    => '/',
+                    'defaults' => array(
+                        'controller' => 'Debug\Controller\Index',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+
+            // using the path /debug/:controller/:action
+            'debug' => array(
+                'type'    => 'Literal',
+                'options' => array(
+                    'route'    => '/debug',
+                    'defaults' => array(
+                        '__NAMESPACE__' => 'Debug\Controller',
+                        'controller'    => 'Index',
+                        'action'        => 'index',
+                    ),
+                ),
+                'may_terminate' => true,
+                'child_routes' => array(
+                    'default' => array(
+                        'type'    => 'Segment',
+                        'options' => array(
+                            'route'    => '/[:controller[/:action]]',
+                            'constraints' => array(
+                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            ),
+                            'defaults' => array(
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        ),
+    ),
+    'service_manager' => array(
+        'factories' => array(
+        ),
+    ),
+    'controllers' => array(
+        'invokables' => array(
+            'Debug\Controller\Index' => 'Debug\Controller\IndexController'
+        ),
+    ),
+    'view_manager' => array(
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
+);
