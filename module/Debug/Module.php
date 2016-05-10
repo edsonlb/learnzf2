d05e39b218f93b9fd4ca62002e6934fe554a5d7a
edsonlb <ti_edson@yahoo.com.br>
2016-05-10 12:16:47 +0200

    Page 31 - Debug on error done

--- module/Debug/Module.php
+++ module/Debug/Module.php
@@ -2,16 +2,49 @@
 
 namespace Debug;
 
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
 use Zend\Mvc\ModuleRouteListener;
+use Zend\ModuleManager\ModuleManager;
+use Zend\ModuleManager\ModuleEvent;
+use Zend\EventManager\Event;
 use Zend\Mvc\MvcEvent;
 
-class Module
-{
+class Module implements AutoloaderProviderInterface{
+
+    public function init(ModuleManager $moduleManager){
+        $eventManager = $moduleManager->getEventManager();
+        $eventManager->attach(ModuleEvent::EVENT_LOAD_MODULES_POST, 
+            array($this, 'loadedModulesInfo'));
+
+    }
+
+    public function loadedModulesInfo(Event $event){
+
+        $moduleManager = $event->getTarget();
+        $loadesModules = $moduleManager->getLoadedModules();
+        error_log(var_export($loadesModules, true));
+    }
+
     public function onBootstrap(MvcEvent $e)
     {
-        $eventManager        = $e->getApplication()->getEventManager();
-        $moduleRouteListener = new ModuleRouteListener();
-        $moduleRouteListener->attach($eventManager);
+        $eventManager = $e->getApplication()->getEventManager();
+        $eventManager->attach(MvcEvent::EVENT_DISPATCH_ERROR, array($this,'handleError'));
+
+        //$moduleRouteListener = new ModuleRouteListener();
+        //$moduleRouteListener->attach($eventManager);
+    }
+
+    public function handleError(MvcEvent $event){
+        $controller = $event->getController();
+        $error = $event->getParam('error');
+        $exception = $event->getParam('exception');
+        $message = sprintf('Error dispatching controller "%s". Error was: "%s"', $controller, $error);
+
+        if ($exception instanceof\Exception){
+            $message .= ', Exception('.$exception->getMessage().'): '.$exception->getTradeAsString();
+        }
+
+        error_log($message);
     }
 
     public function getConfig()
